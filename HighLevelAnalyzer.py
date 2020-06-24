9d46098681aa24f52bedd79d044c30689039c1af
Erik Fleckstein <erik@tinkerforge.com>
2020-06-24 13:26:10 +0200

    Version 2.0.3

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,7 +1,7 @@
 #############################################################
 # This file was automatically generated on 2020-06-24.      #
 #                                                           #
-# Saleae Bindings Version 2.0.2                             #
+# Saleae Bindings Version 2.0.3                             #
 #                                                           #
 # If you have a bugfix for this file and want to commit it, #
 # please fix the bug in the generator. You can find a link  #
